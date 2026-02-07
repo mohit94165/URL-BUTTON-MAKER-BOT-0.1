02ff7e82b80593209fdf0abdbdf047ef003b7856
mohit94165 <rohitgujjar6541@gmail.com>
2026-02-07 10:15:56 +0530

    Fix BOT_TOKEN environment variable syntax

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv(8481849799:AAFakjETXmt6UxK411GSTo36QY6tTUASfZs)
+BOT_TOKEN = os.getenv("8481849799:AAFakjETXmt6UxK411GSTo36QY6tTUASfZs")
 ADMINS = 6728678197  # <-- apna Telegram ID daalo
