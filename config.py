9d98c6812702c1112c53883f893682f6180ef808
mohit94165 <rohitgujjar6541@gmail.com>
2026-02-07 10:22:51 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("8481849799:AAFakjETXmt6UxK411GSTo36QY6tTUASfZs")
-ADMINS = 6728678197  # <-- apna Telegram ID daalo
